4d07227d88fde6663cf887a8a1ebe6968bbde8f2
DemiwizAces <erdmriosnadeau@gmail.com>
2024-03-27 15:14:05 -0400

    made a small change

--- src/main/java/com/otu/SOFE3980U/Trip.java
+++ src/main/java/com/otu/SOFE3980U/Trip.java
@@ -3,14 +3,20 @@ package com.otu.SOFE3980U;
 public class Trip {
 	int totalTripDuration, departingTime;
 	// Made a change from the design to be more consistent: changed types of dpAirpot and FAirport to Airport instead of String
-	Airport departingAirport, FinalDestination;
+	Airport departingAirport, finalDestination;
 	String[] flightPath;
-
-	public Trip() {
-		// TODO Auto-generated constructor stub
-	}
 	
-	void getFlightPaths() {}
+	void getFlightPaths(Airport departingAirport, Airport finalDestination) {
+		this.departingAirport = departingAirport;
+		Airport current = this.departingAirport;
+		
+		
+		current.getConnectingAirports();
+		while(current.getName() != departingAirport.getName()) {
+			current.getConnectingAirports();	
+		}
+		
+	}
 	
 	// Changed name from time to Duration to be more consistent
 	void getFlightDuration(String[] flightPath) {
