598230735e49508de7c465988070bd21c338f08e
Neuron-Chan <rolfsibal@gmail.com>
2024-04-01 19:05:11 -0400

    TripTest.java is born

---
+++ src/test/java/com/otu/SOFE3980U/TripTest.java
@@ -0,0 +1,11 @@
+package com.otu.SOFE3980U;
+
+import static org.junit.Assert.*;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class TripTest {
+    
+}
