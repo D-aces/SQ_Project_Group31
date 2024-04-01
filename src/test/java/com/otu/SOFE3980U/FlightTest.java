d9f9f14f299b542f81ae316e9e42db397e4b824a
Neuron-Chan <rolfsibal@gmail.com>
2024-04-01 17:43:11 -0400

    updated Flight classes

--- src/test/java/com/otu/SOFE3980U/FlightTest.java
+++ src/test/java/com/otu/SOFE3980U/FlightTest.java
@@ -7,21 +7,7 @@ import junit.framework.TestSuite;
 public class FlightTest 
 extends TestCase
 {
-    private Airport departingAirport, destinationAirport;
-	private int departingTime, flightDuration;
-    /**
-	 * Constructor for Flight class
-	 */
-	public Flight(Airport departingAirport, Airport destinationAirport, int departingTime, int flightDuration) {
-		this.departingAirport = Toronto;
-		this.destinationAirport = Beijing;
-		this.departingTime = 40000;
-		this.flightDuration = 68040;
-    }
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public arrivalTimeTest()
+    Flight flight = new Flight("Toronto", "Beijing", 40000, 60840);
+
+    
 }
