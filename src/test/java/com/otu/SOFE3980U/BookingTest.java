82bb41e8f28f86595a57224d2472adcd21a4ca07
Neuron-Chan <rolfsibal@gmail.com>
2024-04-01 17:20:23 -0400

    added tests

---
+++ src/test/java/com/otu/SOFE3980U/BookingTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookingTest {
+
+	// getFlightType requests
+	@Test
+	public void roundtest() {
+		assertEquals("valid", Booking.GetFlightType("Round-Trip"));
+	}
+
+	@Test
+	public void onewaytest() {
+		assertEquals("valid", Booking.GetFlightType("One-Way"));
+	}
+	
+	// getReturnType requests
+	@Test
+	public void roundtest() {
+		assertEquals("valid", Booking.GetFlightType("Round-Trip"));
+	}
+
+	@Test
+	public void onewaytest() {
+		assertEquals("valid", Booking.GetFlightType("One-Way"));
+	}
+	
+	// getFlightType requests
+	@Test
+	public void roundtest() {
+		assertEquals("valid", Booking.GetFlightType("Round-Trip"));
+	}
+
+	@Test
+	public void onewaytest() {
+		assertEquals("valid", Booking.GetFlightType("One-Way"));
+	}
+	
+	// getFlightType requests
+	@Test
+	public void roundtest() {
+		assertEquals("valid", Booking.GetFlightType("Round-Trip"));
+	}
+
+	@Test
+	public void onewaytest() {
+		assertEquals("valid", Booking.GetFlightType("One-Way"));
+	}
+}
