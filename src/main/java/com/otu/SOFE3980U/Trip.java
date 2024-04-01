e40960407ca1ed60427af66873221c497e73cc18
DemiwizAces <erdmriosnadeau@gmail.com>
2024-04-01 15:22:41 -0400

    Added stuff to pom.xml

--- src/main/java/com/otu/SOFE3980U/Trip.java
+++ src/main/java/com/otu/SOFE3980U/Trip.java
@@ -4,8 +4,12 @@ public class Trip {
 	int totalTripDuration, departingTime;
 	// Made a change from the design to be more consistent: changed types of dpAirpot and FAirport to Airport instead of String
 	Airport departingAirport, finalDestination;
-	String[] flightPath;
+	// Made a change to flightPath array --> Changing type to flight makes it easier to get the flight duration 
+	Flight[] flightPath;
 	
+	/*
+	 * This is the tough thing to implement
+	 */
 	void getFlightPaths(Airport departingAirport, Airport finalDestination) {
 		this.departingAirport = departingAirport;
 		Airport current = this.departingAirport;
@@ -17,9 +21,19 @@ public class Trip {
 		}
 		
 	}
-	
+
 	// Changed name from time to Duration to be more consistent
-	void getFlightDuration(String[] flightPath) {
-		
+	int calcFlightDuration(Flight[] flightPath) {
+		for(int x = 0; x < flightPath.length; x++) {
+			totalTripDuration += flightPath[x].getFlightDuration();
+			
+		}
+		return totalTripDuration;
+	}
+	// Changed name from time to Duration to be more consistent
+	int getFlightDuration(Flight[] flightPath) {
+		if(totalTripDuration <= 0)
+			calcFlightDuration(flightPath);
+		return totalTripDuration;
 	}
 }
