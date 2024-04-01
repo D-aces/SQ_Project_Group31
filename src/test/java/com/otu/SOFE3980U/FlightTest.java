56229efb0efa524b3f56951fc0aafc68a63cbe1e
Neuron-Chan <rolfsibal@gmail.com>
2024-04-01 17:37:07 -0400

    please help writing tests are hard

---
+++ src/test/java/com/otu/SOFE3980U/FlightTest.java
@@ -0,0 +1,27 @@
+package com.otu.SOFE3980U;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class FlightTest 
+extends TestCase
+{
+    private Airport departingAirport, destinationAirport;
+	private int departingTime, flightDuration;
+    /**
+	 * Constructor for Flight class
+	 */
+	public Flight(Airport departingAirport, Airport destinationAirport, int departingTime, int flightDuration) {
+		this.departingAirport = Toronto;
+		this.destinationAirport = Beijing;
+		this.departingTime = 40000;
+		this.flightDuration = 68040;
+    }
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public arrivalTimeTest()
+}
