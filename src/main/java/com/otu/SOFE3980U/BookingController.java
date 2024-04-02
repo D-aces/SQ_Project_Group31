1c4373829d747075cb3b752dc6f5a97d5653f158
DemiwizAces <erdmriosnadeau@gmail.com>
2024-04-02 01:10:17 -0400

    forgot to add a past bookings template

--- src/main/java/com/otu/SOFE3980U/BookingController.java
+++ src/main/java/com/otu/SOFE3980U/BookingController.java
@@ -1,5 +1,23 @@
 package com.otu.SOFE3980U;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class BookingController {
+	@GetMapping("/bookflight")
+	public String getBooking() {
+		return null;
+	}
+	
+	// Yeah I'm not sure what type this should be --> DMitri
+	// TODO: Figure out type for the tickets (Is it a string array of flights?)
+	@PostMapping("/generateticket")
+	public String getBooking() {
+		return null;
+	}
 
 }
