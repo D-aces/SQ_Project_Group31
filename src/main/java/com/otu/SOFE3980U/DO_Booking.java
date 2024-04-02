49334056ebc8dc0475335c4ed060ed742eaa5768
noto-21 <128177967+noto-21@users.noreply.github.com>
2024-04-02 13:33:59 -0400

    refactor: Finished commenting DO_Booking

--- src/main/java/com/otu/SOFE3980U/DO_Booking.java
+++ src/main/java/com/otu/SOFE3980U/DO_Booking.java
@@ -40,17 +40,18 @@ public class DO_Booking implements Booking
     //Get the list of flights
     public List<Flight> getFlights()
     {
-        List<Flight> list= new ArrayList<>();
+        List<Flight> list= new ArrayList<>();//Create a new list
 
-        if(this.flight != null)
-            list.add(flight);
+        if(this.flight != null)//If the flight is not null
+            list.add(flight);//Add the flight to the list
 
-        return list;
+        return list;//Return the list
     }
 
+    //Add a flight to the booking
     public void addFlight(Flight origin)
     {      
-        if(flight == null)
-            this.flight = origin;
+        if(flight == null)//If the flight is null
+            this.flight = origin;//Set the flight to the origin
     }
 }
